855a05f0d531627a3fa5e72dace8e140ba4c5577
Bella <annabellaputridirgo@gmail.com>
2024-04-25 14:26:42 +0800

    fix: retrieve originall API structure

---
+++ app/controllers/appv2.rb
@@ -0,0 +1,119 @@
+require 'sequel'
+require 'roda'
+require 'json'
+
+module Cryal
+  class Api < Roda
+    plugin :environments
+    plugin :halt
+    plugin :all_verbs
+
+    route do |routing|
+      response['Content-Type'] = 'application/json'
+
+      routing.root do
+        response.status = 200
+        { message: 'NaviTogether API is up and running!' }.to_json
+      end
+
+      routing.on 'api' do
+        routing.on 'v1' do
+          # User Management Routes
+          routing.on 'users' do
+            routing.on String do |user_id|
+              # GET /api/v1/users/[id]
+              routing.get do
+                begin
+                  output = User.first(user_id: user_id)
+                  response.status = 200
+                  output.to_json
+                rescue StandardError
+                  routing.halt 404, { message: 'Users not found' }.to_json
+                end
+              end
+
+              # GET api/v1/users/
+              routing.get do
+                begin
+                  output = User.all
+                  response.status = 200
+                  output.to_json
+                rescue StandardError
+                  routing.halt 404, { message: 'Fail to retrieve all users' }.to_json
+                end
+              end
+
+              routing.put do
+                # PUT /api/v1/users/:user_id
+                # Update user information
+              end
+
+              routing.delete do
+                # DELETE /api/v1/users/:user_id
+                # Delete a user account
+              end
+            end
+
+            routing.on 'locations' do
+              routing.post do
+                # POST /api/v1/users/:user_id/locations
+                # Post a location update for the user
+              end
+
+              routing.get do
+                # GET /api/v1/users/:user_id/locations
+                # Get location history for the user
+              end
+            end
+          end
+
+          # Room Management Routes
+          routing.on 'rooms' do
+            routing.post do
+              # POST /api/v1/rooms
+              # Create a new room
+            end
+
+            routing.on String do |room_id|
+              routing.get do
+                # GET /api/v1/rooms/:room_id
+                # Retrieve details about a specific room
+              end
+
+              routing.put do
+                # PUT /api/v1/rooms/:room_id
+                # Update room details
+              end
+
+              routing.delete do
+                # DELETE /api/v1/rooms/:room_id
+                # Delete a room
+              end
+
+              routing.on 'join' do
+                routing.post do
+                  # POST /api/v1/rooms/:room_id/join
+                  # Join a room
+                end
+              end
+
+              routing.on 'leave' do
+                routing.post do
+                  # POST /api/v1/rooms/:room_id/leave
+                  # Leave a room
+                end
+              end
+
+              routing.on 'target' do
+                routing.put do
+                  # PUT /api/v1/rooms/:room_id/target
+                  # Set or update the target destination in a room
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
