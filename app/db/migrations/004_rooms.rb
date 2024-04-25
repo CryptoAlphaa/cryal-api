e1adb970f2c7121170a501427e12e49b9358ec4a
Bella <annabellaputridirgo@gmail.com>
2024-04-25 09:21:00 +0800

    fix: Fix foreign key constraint issue in Room creation

--- app/db/migrations/004_rooms.rb
+++ app/db/migrations/004_rooms.rb
@@ -5,7 +5,7 @@ Sequel.migration do
       create_table(:rooms) do
         primary_key :id
         foreign_key :target_id, :targets
-        foreign_key :created_by, :users, null: false
+        foreign_key :user_id, :users, null: false
         String :room_name, null: false
         String :room_password
         DateTime :timestamp, default: Sequel::CURRENT_TIMESTAMP
