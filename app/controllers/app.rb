866df2afe4580d1223e11f5bb76c2dc7d2fb9b50
DarkOracle13 <bobby.setiawan2002@gmail.com>
2024-04-29 21:51:09 +0800

    feat: Refactor app.rb
    Co-authored-by: Bella <annabellaputridirgo@gmail.com>
    Co-authored-by: JReyG <GukGuk25@users.noreply.github.com>

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,229 +1,229 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'sequel'
-require 'roda'
-require 'json'
+# require 'sequel'
+# require 'roda'
+# require 'json'
 
-module Cryal
-  # Class for designing the API
-  class Api < Roda # rubocop:disable Metrics/ClassLength
-    plugin :environments
-    plugin :halt
+# module Cryal
+#   # Class for designing the API
+#   class Api < Roda # rubocop:disable Metrics/ClassLength
+#     plugin :environments
+#     plugin :halt
 
-    route do |routing| # rubocop:disable Metrics/BlockLength
-      response['Content-Type'] = 'application/json'
+#     route do |routing| # rubocop:disable Metrics/BlockLength
+#       response['Content-Type'] = 'application/json'
 
-      routing.root do
-        response.status = 200
-        { message: 'Welcome to Cryal APIs' }.to_json
-      end
+#       routing.root do
+#         response.status = 200
+#         { message: 'Welcome to Cryal APIs' }.to_json
+#       end
 
-      routing.on 'api' do # rubocop:disable Metrics/BlockLength
-        routing.on 'v1' do # rubocop:disable Metrics/BlockLength
-          routing.on 'users' do # rubocop:disable Metrics/BlockLength
-            routing.on String do |user_id| # rubocop:disable Metrics/BlockLength
-              routing.on 'createroom' do
-                # post api/v1/users/[id]/createroom
-                routing.post do
-                  new_data = JSON.parse(routing.body.read)
-                  new_data['user_id'] = user_id.to_i
-                  new_target = Room.new(new_data)
-                  raise('Could not save room') unless new_target.save
+#       routing.on 'api' do # rubocop:disable Metrics/BlockLength
+#         routing.on 'v1' do # rubocop:disable Metrics/BlockLength
+#           routing.on 'users' do # rubocop:disable Metrics/BlockLength
+#             routing.on String do |user_id| # rubocop:disable Metrics/BlockLength
+#               routing.on 'createroom' do
+#                 # post api/v1/users/[id]/createroom
+#                 routing.post do
+#                   new_data = JSON.parse(routing.body.read)
+#                   new_data['user_id'] = user_id.to_i
+#                   new_target = Room.new(new_data)
+#                   raise('Could not save room') unless new_target.save
 
-                  response.status = 201
-                  { message: 'room saved', data: new_target }.to_json
-                rescue StandardError => e
-                  routing.halt 404, { message: 'DB Error', error: e }.to_json
-                end
-              end
+#                   response.status = 201
+#                   { message: 'room saved', data: new_target }.to_json
+#                 rescue StandardError => e
+#                   routing.halt 404, { message: 'DB Error', error: e }.to_json
+#                 end
+#               end
 
-              routing.on 'joinroom' do
-                # post api/v1/users/[id]/joinroom
-                routing.post do
-                  new_data = JSON.parse(routing.body.read)
-                  new_data['user_id'] = user_id.to_i
-                  new_data['active'] = true
-                  new_target = User_Room.new(new_data)
+#               routing.on 'joinroom' do
+#                 # post api/v1/users/[id]/joinroom
+#                 routing.post do
+#                   new_data = JSON.parse(routing.body.read)
+#                   new_data['user_id'] = user_id.to_i
+#                   new_data['active'] = true
+#                   new_target = User_Room.new(new_data)
 
-                  raise('Could Join Room') unless new_target.save
+#                   raise('Could Join Room') unless new_target.save
 
-                  response.status = 201
-                  { message: 'Room Joinned Succesfully', data: new_target }.to_json
-                rescue StandardError => e
-                  routing.halt 404, { message: 'DB Error', error: e }.to_json
-                end
-              end
+#                   response.status = 201
+#                   { message: 'Room Joinned Succesfully', data: new_target }.to_json
+#                 rescue StandardError => e
+#                   routing.halt 404, { message: 'DB Error', error: e }.to_json
+#                 end
+#               end
 
-              routing.on 'location' do # rubocop:disable Metrics/BlockLength
-                routing.on String do |location_id|
-                  # get api/v1/location/[id]
-                  routing.get do
-                    output = Location.first(location_id:)
-                    output.nil? raise 'Location not found'
-                    response.status = 200
-                    output.to_json
-                  rescue StandardError => e
-                    routing.halt 404, { message: 'Location not found', error: e }.to_json
-                  end
-                end
-                # get api/vi/users/[id]/location
-                routing.get do
-                  output = { data: User.first(user_id:).locations }
-                  JSON.pretty_generate(output)
-                rescue StandardError => e
-                  routing.halt 404, { message: 'DB Error', error: e }.to_json
-                end
+#               routing.on 'location' do # rubocop:disable Metrics/BlockLength
+#                 routing.on String do |location_id|
+#                   # get api/v1/location/[id]
+#                   routing.get do
+#                     output = Location.first(location_id:)
+#                     output.nil? raise 'Location not found'
+#                     response.status = 200
+#                     output.to_json
+#                   rescue StandardError => e
+#                     routing.halt 404, { message: 'Location not found', error: e }.to_json
+#                   end
+#                 end
+#                 # get api/vi/users/[id]/location
+#                 routing.get do
+#                   output = { data: User.first(user_id:).locations }
+#                   JSON.pretty_generate(output)
+#                 rescue StandardError => e
+#                   routing.halt 404, { message: 'DB Error', error: e }.to_json
+#                 end
 
-                # post api/vi/users/[id]/location
-                routing.post do
-                  new_data = JSON.parse(routing.body.read)
-                  new_data['cur_lat'] = new_data['cur_lat'].to_f
-                  new_data['cur_long'] = new_data['cur_long'].to_f
-                  new_data['user_id'] = user_id.to_i
-                  new_loc = Location.new(new_data)
+#                 # post api/vi/users/[id]/location
+#                 routing.post do
+#                   new_data = JSON.parse(routing.body.read)
+#                   new_data['cur_lat'] = new_data['cur_lat'].to_f
+#                   new_data['cur_long'] = new_data['cur_long'].to_f
+#                   new_data['user_id'] = user_id.to_i
+#                   new_loc = Location.new(new_data)
 
-                  raise('Could not save Location') unless new_loc.save
+#                   raise('Could not save Location') unless new_loc.save
 
-                  response.status = 201
-                  { message: 'Location saved', data: new_loc }.to_json
-                rescue StandardError => e
-                  routing.halt 404, { message: 'DB Error', error: e }.to_json
-                end
-              end
+#                   response.status = 201
+#                   { message: 'Location saved', data: new_loc }.to_json
+#                 rescue StandardError => e
+#                   routing.halt 404, { message: 'DB Error', error: e }.to_json
+#                 end
+#               end
 
-              # get api/v1/users/[id]
-              routing.get do
-                output = User.first(user_id:)
-                output.nil? raise 'User not found'
-                response.status = 200
-                output.to_json
-              rescue StandardError => e
-                routing.halt 404, { message: 'Users not found', error: e }.to_json
-              end
-            end
+#               # get api/v1/users/[id]
+#               routing.get do
+#                 output = User.first(user_id:)
+#                 # output.nil? raise 'User not found'
+#                 response.status = 200
+#                 output.to_json
+#               rescue StandardError => e
+#                 routing.halt 404, { message: 'Users not found', error: e }.to_json
+#               end
+#             end
 
-            # get api/v1/users
-            routing.get do
-              output = { data: User.all }
-              JSON.pretty_generate(output)
-            rescue StandardError => e
-              routing.halt 404, { message: 'something wrong plis fix get users', errorL: e }.to_json
-            end
+#             # get api/v1/users
+#             routing.get do
+#               output = { data: User.all }
+#               JSON.pretty_generate(output)
+#             rescue StandardError => e
+#               routing.halt 404, { message: 'something wrong plis fix get users', errorL: e }.to_json
+#             end
 
-            # post api/v1/users
-            routing.post do
-              new_data = JSON.parse(routing.body.read)
-              new_user = User.new(new_data)
-              raise('Could not save User') unless new_user.save
+#             # post api/v1/users
+#             routing.post do
+#               new_data = JSON.parse(routing.body.read)
+#               new_user = User.new(new_data)
+#               raise('Could not save User') unless new_user.save
 
-              response.status = 201
-              { message: 'User saved', data: new_user }.to_json
-            rescue StandardError => e
-              routing.halt 404, { message: 'Can\'t load any users', error: e }.to_json
-            end
-          end
-          response.status = 200
-          { message: 'Welcome to Cryal api/v1' }.to_json
+#               response.status = 201
+#               { message: 'User saved', data: new_user }.to_json
+#             rescue StandardError => e
+#               routing.halt 404, { message: 'Can\'t load any users', error: e }.to_json
+#             end
+#           end
+#           response.status = 200
+#           { message: 'Welcome to Cryal api/v1' }.to_json
 
-          routing.on 'rooms' do
-            routing.on String do |room_id|
-              # get api/v1/rooms/[id]
-              routing.get do
-                output = Room.first(room_id:)
-                output.nil? raise 'Room not found'
-                response.status = 200
-                output.to_json
-              rescue StandardError => e
-                routing.halt 404, { message: 'Room not found', error: e }.to_json
-              end
-            end
-            # get api/v1/rooms
-            routing.get do
-              output = { data: Room.all }
-              JSON.pretty_generate(output)
-            rescue StandardError => e
-              routing.halt 404, { message: 'something wrong plis fix get rooms', errorL: e }.to_json
-            end
-          end
+#           routing.on 'rooms' do
+#             routing.on String do |room_id|
+#               # get api/v1/rooms/[id]
+#               routing.get do
+#                 output = Room.first(room_id:)
+#                 output.nil? raise 'Room not found'
+#                 response.status = 200
+#                 output.to_json
+#               rescue StandardError => e
+#                 routing.halt 404, { message: 'Room not found', error: e }.to_json
+#               end
+#             end
+#             # get api/v1/rooms
+#             routing.get do
+#               output = { data: Room.all }
+#               JSON.pretty_generate(output)
+#             rescue StandardError => e
+#               routing.halt 404, { message: 'something wrong plis fix get rooms', errorL: e }.to_json
+#             end
+#           end
 
-          routing.on 'location' do
-            routing.on String do |location_id|
-              # get api/v1/location/[id]
-              routing.get do
-                output = Location.first(location_id:)
-                output.nil? raise 'Location not found'
-                response.status = 200
-                output.to_json
-              rescue StandardError => e
-                routing.halt 404, { message: 'Location not found', error: e }.to_json
-              end
-            end
-            # get api/v1/location
-            routing.get do
-              output = { data: Location.all }
-              JSON.pretty_generate(output)
-            rescue StandardError => e
-              routing.halt 404, { message: 'something wrong plis fix get location', errorL: e }.to_json
-            end
-          end
+#           routing.on 'location' do
+#             routing.on String do |location_id|
+#               # get api/v1/location/[id]
+#               routing.get do
+#                 output = Location.first(location_id:)
+#                 output.nil? raise 'Location not found'
+#                 response.status = 200
+#                 output.to_json
+#               rescue StandardError => e
+#                 routing.halt 404, { message: 'Location not found', error: e }.to_json
+#               end
+#             end
+#             # get api/v1/location
+#             routing.get do
+#               output = { data: Location.all }
+#               JSON.pretty_generate(output)
+#             rescue StandardError => e
+#               routing.halt 404, { message: 'something wrong plis fix get location', errorL: e }.to_json
+#             end
+#           end
 
-          routing.on 'user_rooms' do
-            routing.on String do |user_room_id|
-              # get api/v1/user_room/[id]
-              routing.get do
-                output = User_Room.first(user_room_id:)
-                output.nil? raise 'User_Room not found'
-                response.status = 200
-                output.to_json
-              rescue StandardError => e
-                routing.halt 404, { message: 'User_Room not found', error: e }.to_json
-              end
-            end
-            # get api/v1/userroom
-            routing.get do
-              output = { data: User_Room.all }
-              JSON.pretty_generate(output)
-            rescue StandardError => e
-              routing.halt 404, { message: 'something wrong plis fix get rooms', errorL: e }.to_json
-            end
-          end
+#           routing.on 'user_rooms' do
+#             routing.on String do |user_room_id|
+#               # get api/v1/user_room/[id]
+#               routing.get do
+#                 output = User_Room.first(user_room_id:)
+#                 output.nil? raise 'User_Room not found'
+#                 response.status = 200
+#                 output.to_json
+#               rescue StandardError => e
+#                 routing.halt 404, { message: 'User_Room not found', error: e }.to_json
+#               end
+#             end
+#             # get api/v1/userroom
+#             routing.get do
+#               output = { data: User_Room.all }
+#               JSON.pretty_generate(output)
+#             rescue StandardError => e
+#               routing.halt 404, { message: 'something wrong plis fix get rooms', errorL: e }.to_json
+#             end
+#           end
 
-          routing.on 'targets' do
-            routing.on String do |target_id|
-              # get api/v1/targets/[id]
-              routing.get do
-                output = Target.first(target_id:)
-                output.nil? raise 'Target not found'
-                response.status = 200
-                output.to_json
-              rescue StandardError => e
-                routing.halt 404, { message: 'Target not found', error: e }.to_json
-              end
-            end
+#           routing.on 'targets' do
+#             routing.on String do |target_id|
+#               # get api/v1/targets/[id]
+#               routing.get do
+#                 output = Target.first(target_id:)
+#                 output.nil? raise 'Target not found'
+#                 response.status = 200
+#                 output.to_json
+#               rescue StandardError => e
+#                 routing.halt 404, { message: 'Target not found', error: e }.to_json
+#               end
+#             end
 
-            # get api/v1/targets
-            routing.get do
-              output = { data: Target.all }
-              JSON.pretty_generate(output)
-            rescue StandardError => e
-              routing.halt 404, { message: 'something wrong plis fix get target', errorL: e }.to_json
-            end
+#             # get api/v1/targets
+#             routing.get do
+#               output = { data: Target.all }
+#               JSON.pretty_generate(output)
+#             rescue StandardError => e
+#               routing.halt 404, { message: 'something wrong plis fix get target', errorL: e }.to_json
+#             end
 
-            # post api/v1/targets
-            routing.post do
-              new_data = JSON.parse(routing.body.read)
-              new_target = Target.new(new_data)
-              raise('Could not save Target') unless new_target.save
+#             # post api/v1/targets
+#             routing.post do
+#               new_data = JSON.parse(routing.body.read)
+#               new_target = Target.new(new_data)
+#               raise('Could not save Target') unless new_target.save
 
-              response.status = 201
-              { message: 'Target saved', data: new_target }.to_json
-            rescue StandardError => e
-              routing.halt 404, { message: 'Can\'t load any Target', error: e }.to_json
-            end
-          end
-        end
-        response.status = 200
-      end
-    end
-  end
-end
+#               response.status = 201
+#               { message: 'Target saved', data: new_target }.to_json
+#             rescue StandardError => e
+#               routing.halt 404, { message: 'Can\'t load any Target', error: e }.to_json
+#             end
+#           end
+#         end
+#         response.status = 200
+#       end
+#     end
+#   end
+# end
---
+++ app/controllers/appv1.rb
@@ -0,0 +1,220 @@
+# frozen_string_literal: true
+
+require 'sequel'
+require 'roda'
+require 'json'
+
+module Cryal
+  # Class for designing the API
+  class Api < Roda
+    plugin :environments
+    plugin :halt
+    plugin :json
+
+    route do |routing| # rubocop:disable Metrics/BlockLength
+      response['Content-Type'] = 'application/json'
+
+      routing.root do
+        response.status = 200
+        { message: 'Welcome to Cryal APIs' }.to_json
+      end
+
+      routing.on 'api/v1' do # rubocop:disable Metrics/BlockLength
+        routing.on 'users' do # rubocop:disable Metrics/BlockLength
+          routing.on String do |user_id| # rubocop:disable Metrics/BlockLength
+            routing.is do
+              # GET /api/v1/users/[user_id] DONE
+              routing.get do
+                user_fetch_user(routing, user_id)
+              end
+            end
+
+            routing.on 'locations' do
+              # GET /api/v1/users/[user_id]/locations DONE
+              routing.get do
+                user_fetch_locations(routing, user_id)
+              end
+
+              # POST /api/v1/users/[user_id]/locations DONE
+              routing.post do
+                user_create_location(routing, user_id)
+              end
+            end
+
+            # GET /api/v1/users/[user_id]/rooms # BROKEN, TO DO. FIX MODEL
+            routing.on 'rooms' do
+              routing.get do
+                user_fetch_rooms(routing, user_id)
+              end
+            end
+
+            # POST /api/v1/users/[user_id]/createroom DONE
+            routing.on 'createroom' do
+              routing.post do
+                user_create_room(routing, user_id)
+              end
+            end
+
+            # POST /api/v1/users/[user_id]/joinroom DONE
+            routing.on 'joinroom' do
+              routing.post do
+                user_join_room(routing, user_id)
+              end
+            end
+          end
+
+          # GET /api/v1/users DONE
+          routing.get do
+            global_fetch_users(routing)
+          end
+
+          # POST /api/v1/users DONE
+          routing.post do
+            global_create_user(routing)
+          end
+        end
+
+        # GET /api/v1/rooms DONE
+        routing.on 'rooms' do
+          routing.on String do |room_id|
+            routing.is do
+              # GET /api/v1/rooms/[room_id]
+              routing.get do
+                global_fetch_room(routing, room_id)
+              end
+            end
+          end
+          # GET /api/v1/rooms DONE
+          routing.get do
+            global_fetch_room_all(routing)
+          end
+        end
+
+        routing.on 'userrooms' do
+          # GET /api/v1/userrooms DONE
+          routing.get do
+            global_fetch_userrooms(routing)
+          end
+        end
+      end
+    end
+
+    # targets belom di kerjain
+    # targets belom di kerjain
+    # targets belom di kerjain
+
+    private
+
+    # Naming Convention [Route]_[Task]_[Object]_[AdditionalInfo]
+
+    def user_fetch_user(routing, user_id)
+      output = User.first(user_id:)
+      not_found(routing, 'User not found') if output.nil?
+      response.status = 200
+      output.to_json
+    end
+
+    def user_fetch_locations(routing, user_id)
+      output = { data: User.first(user_id:).locations }
+      not_found(routing, 'DB Error') if output.nil?
+      response.status = 200
+      output.to_json
+    end
+
+    def user_create_location(routing, user_id) # rubocop:disable Metrics/AbcSize
+      user = User.first(user_id:)
+      not_found(routing, 'User not found') if user.nil?
+      location = JSON.parse(routing.body.read)
+      location['cur_lat'] = location['cur_lat'].to_f
+      location['cur_long'] = location['cur_long'].to_f
+      location['user_id'] = user_id.to_i
+      location = Location.new(location)
+
+      if location.save
+        response.status = 201
+        { message: 'Location saved', data: location }.to_json
+      else
+        internal_server_error('Could not save Location')
+      end
+    end
+
+    # TODO : Fix model first
+    def user_fetch_rooms(routing, user_id)
+      output = { data: User.first(user_id:).rooms }
+      not_found(routing, 'DB Error') if output.nil?
+      response.status = 200
+      output.to_json
+    end
+
+    def user_create_room(routing, user_id)
+      user = User.first(user_id:)
+      not_found(routing, 'User not found') if user.nil?
+      room = JSON.parse(routing.body.read)
+      room['user_id'] = user_id.to_i
+      room = Room.new(room)
+      if room.save
+        response.status = 201
+        { message: 'Room saved', data: room }.to_json
+      else
+        internal_server_error('Could not save Room')
+      end
+    end
+
+    def user_join_room(routing, user_id)
+      user = User.first(user_id:)
+      not_found(routing, 'User not found') if user.nil?
+      user_room = JSON.parse(routing.body.read)
+      user_room['user_id'] = user_id.to_i
+      user_room['active'] = true
+      user_room = User_Room.new(user_room)
+      if user_room.save
+        response.status = 201
+        { message: 'Room Join Successfully', data: user_room }.to_json
+      else
+        internal_server_error('Could not save User_Room')
+      end
+    end
+
+    def global_create_user(routing)
+      user = JSON.parse(routing.body.read)
+      user = User.new(user)
+      if user.save
+        response.status = 201
+        { message: 'User saved', data: user }.to_json
+        user.to_json
+      else
+        internal_server_error('Failed to create user')
+      end
+    end
+
+    def global_fetch_users(_routing)
+      output = { data: User.all }
+      output.to_json
+    end
+
+    def global_fetch_room(routing, room_id)
+      output = Room.first(room_id:)
+      not_found(routing, 'Room not found') if output.nil?
+      response.status = 200
+      output.to_json
+    end
+
+    def global_fetch_room_all(_routing)
+      output = { data: Room.all }
+      output.to_json
+    end
+
+    def global_fetch_userrooms(_routing)
+      output = { data: User_Room.all }
+      output.to_json
+    end
+
+    def not_found(routing, message)
+      routing.halt 404, { message: }.to_json
+    end
+
+    def internal_server_error(message)
+      routing.halt 500, { message: }.to_json
+    end
+  end
+end
